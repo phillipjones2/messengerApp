137175950e16dc8897c4cdecb09be35278f5c66c
Phillip Jones <phillipemersonjones@gmail.com>
2016-06-22 00:44:38 -0500

    change messages app to blog app

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,6 @@ class Message < ActiveRecord::Base
 	has_many :comments
 
 	validates :name, presence: true
-	validates :email, presence: true,
-		format: { :with => /\A[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}\z/i }
 	validates :content, presence: true
 
 end
