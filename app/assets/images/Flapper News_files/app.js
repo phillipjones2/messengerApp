e31d9a4a27a6a0333d538a649311c6695bb27fcc
Phillip Jones <phillipemersonjones@gmail.com>
2016-06-25 09:32:10 -0500

    fixed nav on blog message page

---
+++ app/assets/images/Flapper News_files/app.js
@@ -0,0 +1,58 @@
+angular.module('flapperNews', ['ui.router'])
+.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    });
+    // .state('posts', {
+    //   url: '/posts/{id}',
+    //   templateUrl: '/posts.html',
+    //   controller: 'PostsCtrl'
+    //   });
+  $urlRouterProvider.otherwise('home');
+}])
+.factory('posts', [function(){
+  var o = {
+  posts: []
+};
+return o;
+}])
+
+.controller('MainCtrl', [
+'$scope',
+'posts',
+function($scope, posts){
+  $scope.posts = posts.posts;
+
+$scope.incrementUpvotes = function(post) {
+  post.upvotes += 1;
+};
+$scope.addPost = function(){
+  if(!$scope.title || $scope.title === '') { return; }
+  $scope.posts.push({
+    title: $scope.title,
+    link: $scope.link,
+    upvotes: 0,
+    comments: [
+    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+  ]
+  });
+  $scope.title = '';
+  $scope.link = '';
+};
+}])
+
+.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+  $scope.post = posts.posts[$stateParams.id];
+}]);
