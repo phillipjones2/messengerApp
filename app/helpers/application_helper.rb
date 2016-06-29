446a33ffec8215abc369350146e0ff213399bf21
Phillip Jones <phillipemersonjones@gmail.com>
2016-06-29 10:02:48 -0500

    refactor titles with helper function

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,5 +14,14 @@ module ApplicationHelper
       nil
     end
 
+    # Returns the full title on a per-page basis.
+    def full_title(page_title = '')
+      base_title = "Phillip Emerson Jones portfolio"
+      if page_title.empty?
+        base_title
+      else
+        "PEJ | " + page_title
+      end
+    end
 
 end
